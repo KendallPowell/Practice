0b99c2a1db0d229a3105a5d087e1af7acfd5b3a8
Kendall Powell <kendalpowel@hotmail.com>
2023-01-16 22:02:55 -0700

    struggling with how Csharp works

--- Models/Dino.cs
+++ Models/Dino.cs
@@ -1,6 +1,18 @@
-namespace dinoHotel.Models;
+namespace dinoTamed.Models;
 
-public class dinoHotel
+public class Dino
 {
+  public Dino(int id, string name, int level, string type);
+{
+  Id = id;
+  Name = name;
+  Level = level;
+  Color = color;
+  
+}
+public int Id { get; set; }
+public string Name { get; set; }
+public int Age { get; set; }
+public string Color { get; set; }
 
 }
